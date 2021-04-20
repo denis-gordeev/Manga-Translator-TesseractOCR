e2a0e6b4b276a8da769e3c13167d3425d5a9bb2b
Denis Gordeev <gordeev-di@ranepa.ru>
2021-04-20 17:11:52 +0300

    I have added requirements.txt, changed brackets to make it work with Python3, added changes to make it work with OpenCV > 4.5 and fixed google-translate issues.

--- manga_translate.py
+++ manga_translate.py
@@ -10,42 +10,42 @@ import typeset
 
 def main():
 
-    imgPath = 'img'
-    transPath = 'translated'
+    imgPath = "img"
+    transPath = "translated"
 
-    images  = load_images_from_folder(imgPath)
-    print ('Processing images:')
+    images = load_images_from_folder(imgPath)
+    print("Processing images:")
     names = load_names_from_folder(imgPath)
-    print ('JAPANESE' + ' ' + '->' + ' ' + 'VIETNAMESE')
+    print("JAPANESE" + " " + "->" + " " + "VIETNAMESE")
 
     transImg = []
     i = 0
 
     for img in images:
-            blurbs = locate_bubbles.get_blurbs(img)
-            needTransImg = Image.fromarray(img.copy())
+        blurbs = locate_bubbles.get_blurbs(img)
+        needTransImg = Image.fromarray(img.copy())
 
-            for blurb in blurbs:
-                translated = translate.translate_blurb(blurb)
-                typeset.typeset_blurb(needTransImg, translated)
+        for blurb in blurbs:
+            translated = translate.translate_blurb(blurb)
+            typeset.typeset_blurb(needTransImg, translated)
 
-            transImg.append(needTransImg)
+        transImg.append(needTransImg)
 
     for trans in transImg:
-            saveFile = os.path.join(transPath, names[i])
-            trans.save(saveFile)
-            i = i + 1
+        saveFile = os.path.join(transPath, names[i])
+        trans.save(saveFile)
+        i = i + 1
 
-    print ('Statistic:')
-    print transImg
-    print ('Number of translated pages:' + str(i))
+    print("Statistic:")
+    print(transImg)
+    print("Number of translated pages:" + str(i))
 
 
 def load_images_from_folder(folder):
     images = []
 
     for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
+        img = cv2.imread(os.path.join(folder, filename))
         if img is not None:
             images.append(img)
 
@@ -58,7 +58,7 @@ def load_names_from_folder(folder):
     for filename in os.listdir(folder):
         if filename is not None:
             names.append(filename)
-            print filename
+            print(filename)
 
     return names
 
